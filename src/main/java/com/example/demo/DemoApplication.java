d24edd28efc916c4e087d36b98df266694ebcc4a
Danny Thomas <dannyt@netflix.com>
2023-05-10 13:26:28 +1000

    Autowired values for bean interface

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.*;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -19,35 +17,38 @@ public class DemoApplication {
     }
 
     @Bean
-    public static BeanDefinitionRegistryPostProcessor postProcessor() {
-        // Our real implementation walks all registered beans and registers definitions on demand based use of this annotation
-        return new BeanDefinitionRegistryPostProcessor() {
-            @Override
-            public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-                String beanName = "GrpcClient_myclient";
-                // Conditional because this runs again and duplicates the AOT registered bean
-                if (!registry.containsBeanDefinition(beanName)) {
-                    AbstractBeanDefinition beanDefinition = BeanDefinitionBuilder
-                            .genericBeanDefinition(GrpClient.class)
-                            .getBeanDefinition();
-                    beanDefinition.addQualifier(new AutowireCandidateQualifier(GrpcSpringClient.class, "myclient"));
-                    registry.registerBeanDefinition("GrpcClient_myclient", beanDefinition);
-                }
-            }
-
-            @Override
-            public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-            }
-        };
+    public S3ClientConfiguration s3ClientConfiguration() {
+        return new S3ClientConfigurationImpl();
     }
 
-    static class GrpClient {
-
+    @Bean
+    public static AmazonS3 s3Client(S3ClientConfiguration config) {
+        Objects.requireNonNull(config.getRegion());
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(config.getRegion())
+                .build();
     }
 
-    @Component
-    static class RequiresGrpcClient {
-        RequiresGrpcClient(@GrpcSpringClient("myclient") GrpClient client) {
+    interface S3ClientConfiguration {
+        String getBucket();
+
+        String getRegion();
+    }
+
+    static class S3ClientConfigurationImpl implements S3ClientConfiguration {
+
+        @Value("${com.example.demo.s3.bucket:}")
+        private String bucket;
+
+        @Value("${EC2_REGION:}")
+        private String ec2Region;
+
+        public String getRegion() {
+            return ec2Region;
+        }
+
+        public String getBucket() {
+            return bucket;
         }
     }
 
